fcb3a3cd19b57792678dd9b16f7bd50cd59a20a5
asorokin <aleksandr.sorokin@extraenergy.com.cy>
2022-11-08 19:52:21 +0200

    Stub controllers and model objects were added for all 3 spring boot apps

---
+++ movie-info-service/src/main/java/com/elster/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,16 @@
+package com.elster.movieinfoservice.resources;
+
+import com.elster.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "Star Wars");
+    }
+}
